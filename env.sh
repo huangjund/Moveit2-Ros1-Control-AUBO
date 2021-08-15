7e1de32d4028ea1c3fe26daebdaabca455b2f9a0
Junda Huang <1124345597@qq.com>
2021-08-15 20:26:29 +0800

    changed moveit path

--- env.sh
+++ env.sh
@@ -2,5 +2,5 @@
 
 
 source /opt/ros/foxy/setup.bash
-source ~/ws_moveit2/install/setup.bash
+source ~/projects/ws_moveit2/install/setup.bash
 source ~/projects/Moveit2-Ros1-Control-AUBO/aubo_ros2_moveit2_ws/install/setup.bash
